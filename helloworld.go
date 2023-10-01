a86914b436ea8d1c6809272e7051221419aef898
bobo526 <bobo584177909@sina.com>
2023-10-01 23:59:42 +0800

    send slack message

--- helloworld.go
+++ helloworld.go
@@ -7,19 +7,19 @@ import (
 func main(){
 	r := gin.Default()
 
-	r,GET("/hello",func(c *.gin.Context){
+	r.GET("/hello", func(c * gin.Context){
 		c.String(200,"Hello World")
 	})
 
-	api := r.Group("api")
+	api := r.Group("/api")
 
-	api.GET("/ping",func(c*gin.Conmtext){
+	api.GET("/ping", func(c * gin.Context){
 		c.JSON(200,gin.H{
 			"message":"pong",
 		})
 	})
 
-	r.Use(static.Serve("/",static.LocalFile("./views",true)))
+	r.Use(static.Serve("/", static.LocalFile("./views",true)))
 
 	r.Run()
 }
